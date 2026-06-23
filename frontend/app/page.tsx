6652a1d9617c3321ba3da888f1e5b61b015c8551
lollyxsrinand <srinand.com@gmail.com>
2026-06-23 21:59:37 +0530

    frontend: add button no redirect

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link"
 import { redirect } from "next/navigation"
 
 export default function Home() {
-  return redirect(`/products`)
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-neutral-950">
+      <Link 
+      href="/products"
+      className="rounded-2xl px-4 py-2 border border-neutral-700 bg-neutral-800">show products</Link>
+    </div>
+  )
+  // return redirect(`/products`)
 }
